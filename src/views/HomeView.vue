1f0cde6953ad59086d78b7a6fd334b09e8736c78
Pavel Novikov <spl.novikov@gmail.com>
2024-02-23 19:55:53 +0300

    options api, ts example

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="home">Home Page</div>
+  <h1 class="home">Home Page (Options API)</h1>
+  <p>{{ name }} - {{ age }}</p>
+  <button type="button" @click="changeName('aa')">Change Name</button>
+  <button type="button" @click="changeAge(22)">Change Age</button>
 </template>
 
 <script lang="ts">
@@ -8,5 +11,19 @@ import { defineComponent } from "vue";
 export default defineComponent({
   name: "HomeView",
   components: {},
+  data() {
+    return {
+      name: "Link",
+      age: 25 as string | number,
+    };
+  },
+  methods: {
+    changeName(name: string) {
+      return (this.name = name);
+    },
+    changeAge(age: number | string) {
+      return (this.age = age);
+    },
+  },
 });
 </script>
